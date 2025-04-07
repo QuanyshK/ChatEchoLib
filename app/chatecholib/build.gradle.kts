8d19ac55a224d9c738b6d202da6f4d4c883a4191
QuanyshK <zoomer.qurbany@gmail.com>
2025-04-08 01:40:13 +0500

    Initial commit

---
+++ app/chatecholib/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    id ("maven-publish")
+}
+
+
+android {
+    namespace = "com.example.chatecholib"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 29
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.github.quanyshk"
+            artifactId = "chatecholib"
+            version = "0.0.0"
+            afterEvaluate { from(components["release"]) }
+        }
+    }
+}
+repositories {
+    maven {
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/quanyshk/chatecholib")
+    }
+}
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.retrofit)
+    implementation(libs.gson.converter)
+    implementation(libs.okhttp)
+    implementation(libs.logging)
+
+}
